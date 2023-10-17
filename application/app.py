37b7722452b4a74ca71883038f28f15431ff4456
Mehdi Mohammadi <mehdi.mka@gmail.com>
2023-10-17 15:52:11 -0700

    fix CORS

--- application/app.py
+++ application/app.py
@@ -34,9 +34,9 @@ def home():
 
 @app.after_request
 def after_request(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
     return response
 
 if __name__ == "__main__":
